d0b1b312f9538769bf90d54703d807352ae13852
Greg Swallow <gswallow@ivytech.edu>
2018-04-12 19:31:26 -0400

    Move ansible demo to separate, public project

---
+++ ansible/spec/localhost/basic_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe package('git-core') do
+  it { should be_installed }
+end
+
+describe package('ansible') do
+  it { should be_installed }
+end
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe file('/var/www/html/index.html') do
+  its(:content) { should match /Automation for the People/ }
+end
